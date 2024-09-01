ed273d574ed2225447f345fab738386d37100b19
ChumisaUkhoHaya <chumisahaya0@gmail.com>
2024-09-01 17:37:36 +0200

    Update main.jsx

--- form-handling-react/src/main.jsx
+++ form-handling-react/src/main.jsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'https://github.com/ChumisaUkhoHaya/alx-fe-reactjs/blob/main/form-handling-react/node_modules/@types/react'
+import { createRoot } from 'https://github.com/ChumisaUkhoHaya/alx-fe-reactjs/blob/main/form-handling-react/node_modules/@types/react-dom/client.d.ts'
+import App from 'https://github.com/ChumisaUkhoHaya/alx-fe-reactjs/blob/a9e7ba08a5bc27dcc2a30baf952df45c8b728842/form-handling-react/src/App.jsx'
+import 'https://github.com/ChumisaUkhoHaya/alx-fe-reactjs/blob/4dba59e7bcc23eebfa9727b46877c2a3ea0a74d5/form-handling-react/src/index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
