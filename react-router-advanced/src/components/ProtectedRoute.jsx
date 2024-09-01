535add6a3bf7c4f5dce66310467ded9ca9203fdb
ChumisaUkhoHaya <chumisahaya0@gmail.com>
2024-09-01 23:46:16 +0200

    Update ProtectedRoute.jsx

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,6 +1,7 @@
 // src/components/ProtectedRoute.jsx
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+"useAuth"
 
 // Simulate authentication status
 const isAuthenticated = false; // Change to true to simulate logged-in state
