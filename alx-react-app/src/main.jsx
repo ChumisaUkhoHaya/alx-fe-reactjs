227579cca2fd621104896085d34ddb5f50c0838a
ChumisaUkhoHaya <chumisahaya0@gmail.com>
2024-09-01 16:10:48 +0200

    Update main.jsx

--- alx-react-app/src/main.jsx
+++ alx-react-app/src/main.jsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'https://github.com/ChumisaUkhoHaya/alx-fe-reactjs/blob/main/alx-react-app/node_modules/react'
-import { createRoot } from 'https://github.com/ChumisaUkhoHaya/alx-fe-reactjs/blob/main/alx-react-app/node_modules/react-dom'
+import { createRoot } from 'https://github.com/ChumisaUkhoHaya/alx-fe-reactjs/blob/main/alx-react-app/node_modules/react-dom/client.js'
 import App from 'https://github.com/ChumisaUkhoHaya/alx-fe-reactjs/blob/58df284b7596c96973876224f015631cf246f0e8/alx-react-app/src/App.jsx'
 import 'https://github.com/ChumisaUkhoHaya/alx-fe-reactjs/blob/e6a49b7934626cf5ea96d9aa12b8d774505651d6/alx-react-app/src/index.css'
 
