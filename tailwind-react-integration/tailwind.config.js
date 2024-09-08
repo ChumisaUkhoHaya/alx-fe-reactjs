62d1ae61d9f42f66b86be76486067bdd355924a6
ChumisaUkhoHaya <chumisahaya0@gmail.com>
2024-09-08 20:22:37 +0200

    changes

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "https://github.com/ChumisaUkhoHaya/alx-fe-reactjs/blob/main/tailwind-react-integration/src",
+    "https://github.com/ChumisaUkhoHaya/alx-fe-reactjs/blob/main/tailwind-react-integration/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
