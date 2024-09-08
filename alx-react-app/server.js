9f9c528254220aff5383abbcfaf585e122ad8c32
ChumisaUkhoHaya <chumisahaya0@gmail.com>
2024-09-08 20:31:25 +0200

    chamges

--- alx-react-app/server.js
+++ alx-react-app/server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+  origin: "http://localhost:5173"
+}));
 
 app.get('/example', (req, res) => {
   res.json({ message: 'CORS enabled!' });
