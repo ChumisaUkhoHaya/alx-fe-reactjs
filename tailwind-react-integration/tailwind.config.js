23d7313568034de541894bd89828d178f454add7
ChumisaUkhoHaya <chumisahaya0@gmail.com>
2024-09-08 20:53:18 +0200

    Update tailwind.config.js

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -4,8 +4,12 @@ export default {
     "https://github.com/ChumisaUkhoHaya/alx-fe-reactjs/blob/main/tailwind-react-integration/src",
     "https://github.com/ChumisaUkhoHaya/alx-fe-reactjs/blob/main/tailwind-react-integration/index.html"
   ],
+  darkMode: 'media', // Set to 'media' for automatic dark mode based on user preference, or 'class' for manual control
   theme: {
     extend: {},
+  },
+  variants: {
+    extend: {}, // Extend variants for existing utilities if needed
   },
   plugins: [],
 }
