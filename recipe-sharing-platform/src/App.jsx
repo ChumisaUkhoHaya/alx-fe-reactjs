f6ac13984df7d8c6bc0b55ee9e46b4c8ac334756
ChumisaUkhoHaya <chumisahaya0@gmail.com>
2024-09-15 18:25:03 +0200

    update

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import RecipeDetail from './components/RecipeDetail';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -31,6 +34,13 @@ function App() {
       <div className="text-blue-500">
       <h1>Hello, Tailwind CSS!</h1>
     </div>
+
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
     </>
   )
 }
