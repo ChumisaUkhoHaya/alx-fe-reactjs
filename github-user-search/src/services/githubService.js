7d99fae5d58f5052af89758607f3441972318b5d
ChumisaUkhoHaya <chumisahaya0@gmail.com>
2024-09-22 16:43:10 +0200

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,7 +2,12 @@ import axios from 'axios';
 
 const API_URL = 'https://api.github.com/users';
 
-export const fetchUser = async (username) => {
+/**
+ * Fetch user data from GitHub API
+ * @param {string} username - The GitHub username to search for
+ * @returns {Promise<object>} - User data from GitHub
+ */
+export const fetchUserData = async (username) => {
   try {
     const response = await axios.get(`${API_URL}/${username}`);
     return response.data;
@@ -10,3 +15,4 @@ export const fetchUser = async (username) => {
     throw new Error('User not found');
   }
 };
+
